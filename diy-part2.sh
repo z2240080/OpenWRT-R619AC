65bc4394aae35c8375aeedb1ce4ad8bc9e1f845b
z2240080 <71871351+z2240080@users.noreply.github.com>
2022-12-02 22:18:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 # Modify default THEME
 # sed -i 's/luci-theme-bootstrap/luci-theme-argonv3/g' ./feeds/luci/collections/luci/Makefile
 # Modify default PASSWORD
